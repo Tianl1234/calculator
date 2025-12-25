02c8b2eda009f54463bc13c2e95b101a60f54ae3
Tianl1234 <tianlezhou12@gmail.com>
2025-12-25 19:12:27 +0100

    hacker_calc.py

--- hacker_calc.py
+++ hacker_calc.py
@@ -1,1 +1,1 @@
-while (i := input("Rechnung(oder Enter zum Beenden):")): print(eval(i))
+while (i := input("Rechnung(oder Enter zum Beenden):")): print(eval(i) if i[0].isdigit() or i[0] in "(-"else "ungültig")
